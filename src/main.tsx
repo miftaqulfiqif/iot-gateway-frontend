de096f88eaf8e1b70be679e9faec2fe41cfa8646
miftaqulfiqif <miftaqulfiqifirmansyah131@gmail.com>
2025-05-09 16:34:43 +0700

    create ui devices

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import Devices from "./pages/DevicesPage.tsx";
 import Dashboard from "./pages/DashboardPage.tsx";
 import SettingsPage from "./pages/SettingsPage.tsx";
 import DeviceIDAPage from "./pages/Devices/DeviceIDAPage.tsx";
+import DeviceBMIPage from "./pages/Devices/DeviceBMIPage.tsx";
+import DeviceDigitProBabyPage from "./pages/Devices/DeviceDigitProBabyPage.tsx";
+import DeviceDopplerPage from "./pages/Devices/DeviceDopplerPage.tsx";
+import MeasurementPage from "./pages/Devices/MeasurementPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -14,9 +18,16 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<Dashboard />} />
         <Route path="/devices" element={<Devices />} />
         <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/measurement" element={<MeasurementPage />} />
 
         {/* DevicePages */}
         <Route path="/device/digit-pro-ida" element={<DeviceIDAPage />} />
+        <Route path="/device/bmi" element={<DeviceBMIPage />} />
+        <Route
+          path="/device/digit-pro-baby"
+          element={<DeviceDigitProBabyPage />}
+        />
+        <Route path="/device/doppler" element={<DeviceDopplerPage />} />
       </Routes>
     </Router>
   </StrictMode>
